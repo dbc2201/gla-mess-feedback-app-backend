fa1af44493dced11c960d1479ace238aae55dce1
dbc2201 <divyanshb22@gmail.com>
2024-03-05 11:32:07 +0530

    initial commit

---
+++ src/main/java/in/ac/gla/iet/cea/mess/feedback/app/backend/GlaMessFeedbackAppBackendApplication.java
@@ -0,0 +1,13 @@
+package in.ac.gla.iet.cea.mess.feedback.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlaMessFeedbackAppBackendApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GlaMessFeedbackAppBackendApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/in/ac/gla/iet/cea/mess/feedback/app/backend/GlaMessFeedbackAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ac.gla.iet.cea.mess.feedback.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlaMessFeedbackAppBackendApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
